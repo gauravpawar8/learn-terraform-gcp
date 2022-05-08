f8b596c34c51321ed0090cf7ce881556355f5162
Pawar Gaurav (uia72240) <uia72240@ozl1996u.conti.de>
2022-05-09 04:08:06 +0530

    added main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.73.0"
+    }
+  }
+
+  required_version = ">= 0.15.0"
+}
+
+provider "google" {
+  credentials = file("terraformtutorial-349615-776d86451a5c.json")
+  project     = "terraformtutorial-349615"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network1"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+
